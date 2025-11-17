1f25435df3acbb3dc01456a8d7d99a563ff12574
Ankit <2024ucp1794@mnit.ac.in>
2025-11-17 15:27:06 +0530

    Add vector code and readme

---
+++ dotproduct.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a[3] = {1.0, 3.0, -5.0};
+    double b[3] = {4.0, -2.0, -1.0};
+
+    double sum = 0.0;
+
+    for (int i = 0; i < 3; i++) {
+        sum += a[i] * b[i];
+    }
+
+    cout << "Dot product: " << sum << endl;
+
+    return 0;
+}
+
